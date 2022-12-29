b235c508e2f6c5f465997194f99d057f94939a23
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-12-30 01:41:51 +0530

    front-end was completed

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  console.log("_app loaded.")
   return <Component {...pageProps} />
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Head from "next/head";
+import NavBar from "./components/NavBar";
+import Link from "next/link";
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Hello World</title>
-        
+        <title>Home</title>
       </Head>
-      <main >
-        <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      <main>
+        <NavBar className="block" />
+        <div className="  mx-auto h-screen my-auto flex justify-center items-center">
+          <div className="h-1/4 w-1/3 bg-white p-4 border-2 border-black shadow-lg shadow-black rounded-xl max-w-full flex justify-center items-center ">
+            
+            <div className=" font-bold text-2xl max-h-full max-w-full">
+              <Link href="/auth/signup" className="uppercase text-bold border-4 border-x-black  ">register</Link>
+            </div>
+          </div>
+        </div>
       </main>
     </>
-  )
+  );
 }
