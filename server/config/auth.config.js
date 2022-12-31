9f35b7920458436d1bad45574ac369c6f83382aa
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-12-31 16:20:56 +0530

    update

--- server/config/auth.config.js
+++
@@ -1,8 +0,0 @@
-if(process.env.NODE_ENV !== "production"){
-    require("dotenv").config()
-}
-module.exports = {
-    googleClient : process.env.GOOGLE_ID,
-    googleSecret : process.env.GOOGLE_SECRET,
-    callBackUrl  : process.env.CALLBACK_URL
-};
--- server/config/db.config.js
+++
@@ -1,6 +0,0 @@
-if(process.env.NODE_ENV !== "production"){
-    require("dotenv").config()
-}
-module.exports = {
-    DB_URL : process.env.DB_URL
-};
